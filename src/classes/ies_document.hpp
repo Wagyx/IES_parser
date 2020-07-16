a19d73bdf595ac797e84e15d1a152b9578c60497
Antonio Natilla <antonio.natilla@studenti.unimi.it>
2020-07-16 18:28:42 +0200

    Parsing and command-line positional arguments test.
    Parsing of the first line is simple and worked out as an interesting
    test for positional arguments.
    
    You can now compile this and execute it like:
    	ies-parser <path-to-ies-file>
    
    Note that the path has to be specified from current working directory.

---
+++ src/classes/ies_document.hpp
@@ -0,0 +1,8 @@
+enum struct IES_Standard : unsigned int {IES86 = 86, IES91 = 91, IES95 = 95};
+
+class IES_Document
+{
+	public:
+    IES_Standard standard;
+
+};
--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,27 @@
 namespace po = boost::program_options;
 
 #include <iostream>
+#include <fstream>
 #include <iterator>
 #include <fmt/core.h>
 
 #include "classes/custom_formatter.hpp"
+#include "classes/ies_document.hpp"
 
 int main(int argc, char* argv[]) {
     try {
-        po::options_description descr("Allowed options");
+        po::options_description descr("Allowed options:");
         descr.add_options()
             ("help", "producing help message")
-            ("compression", po::value<double>(), "set compression level")
+            ("target", po::value<std::string>(), "specifying the file to be parsed")
+            ("print-to", po::value<std::string>(), "specifying where the parsing result should be printed to")
         ;
 
+        po::positional_options_description target;
+        target.add("target", 1);
+
         po::variables_map varMap;
-        po::store(po::parse_command_line(argc, argv, descr), varMap);
+        po::store(po::command_line_parser(argc, argv).options(descr).positional(target).run(), varMap);
         po::notify(varMap);
 
         if (varMap.count("help")) {
@@ -24,11 +30,34 @@ int main(int argc, char* argv[]) {
             return 0;
         }
 
-        if (varMap.count("compression")) {
-            auto c = varMap["compression"].as<double>();
-            fmt::print("Compression level was set to {}\n", c);
+        if (varMap.count("target")) {
+            auto c = varMap["target"].as<std::string>();
+            std::ifstream file;
+            std::stringstream sstream;
+
+            file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+            file.open(c);
+            sstream << file.rdbuf();
+            std::string data = sstream.str();
+            std::string first = data.substr(0, data.find_first_of('\n'));
+            
+            IES_Document doc;
+            if (first == "IESNA:LM-63-1995")
+                doc = IES_Document { IES_Standard::IES95 };
+            else if (first == "IESNA91")
+                doc = IES_Document { IES_Standard::IES91 };
+            else
+                doc = IES_Document { IES_Standard::IES86 };
+
+            fmt::print("Document's standard is IESNA{}", static_cast<unsigned int>(doc.standard));
+
         } else {
-            fmt::print("Compression level was not set.\n");
+            fmt::print("No target to parse was specified.\n");
+        }
+
+        if (varMap.count("print-to")) {
+            //  TODO: retrieve parameter and print to file
+            fmt::print("print-to function is WIP - Not Implemented Yet\n");
         }
     }
     catch (std::exception& e) {
