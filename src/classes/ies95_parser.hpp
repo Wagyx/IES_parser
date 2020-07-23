1d355a62739ad07e348145a82be2da6ab82a44e8
Antonio Natilla <antonio.natilla@studenti.unimi.it>
2020-07-23 11:33:05 +0200

    Label parsing for all the implemented parsers.
    TODO: Create a wrapper for the unordered_map in <ies_document.hpp> so
    that:
    	- the string_views can be accessed and added to the vector in a
    	  transparent way
    	- the string_views can be "cleaned" from trailing and leading
    	  whitespaces without calling a "clean" lambda explicitly like
    	  in the current implementation
    	- the algorithm that fills it can result clearer

--- src/classes/ies95_parser.hpp
+++ src/classes/ies95_parser.hpp
@@ -18,6 +18,32 @@ public:
         label_line = document.get_lines().cbegin() + 1;
     }
 
-    void parse_label(IES_Document& document) {
+    void parse_label(IES_Document& document) {                                          //TODO: cleanup the values from leading
+                                                                                        // and trailing whitespaces
+        for (auto line = label_line; line != tilt_line; ++line) {
+            auto [square1, square2] = find_key_delimiters(line);
+            auto key                = line->substr(square1 + 1, square2 - square1 - 1);
+            auto value              = line->substr(square2 + 1);
+
+            std::vector<std::string_view> values { value };
+
+            for (auto other_line = line + 1; other_line != tilt_line; ++other_line) {
+                auto index = other_line->find("[MORE]");
+                if (index != other_line->npos) {
+                    auto another_value = other_line->substr(index + 6);
+                    values.push_back(another_value);
+                    line = other_line;
+                } else {
+                    break;
+                }
+            }
+
+            document.labels.emplace(key, values);
+        }
+    }
+
+    private:
+    std::pair<size_t, size_t> find_key_delimiters(std::vector<std::string_view>::const_iterator line) {
+        return std::make_pair(line->find("["), line->find("]"));
     }
 };
